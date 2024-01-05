43145b8ea6db5ce51ebbc808fce24fdbdb77e0b4
Teodor Janez Podobnik <teo.podobnik1234@gmail.com>
2024-01-05 13:52:27 +0100

    namespaces-cgroups-etc directory for low-level structure analysis

--- main.py
+++ main.py
@@ -10,6 +10,21 @@ cli = docker.from_env()
 
 # C container
 start = time.time()
-container = cli.containers.run("dorkamotorka/hello-world-c", ["/app/hello.out"])
+container = cli.containers.run("dorkamotorka/hello-world-c", detach=True)
 end = time.time()
-log.info("Time took to execute the container: %s seconds." % (end - start))
+log.info("Time took to execute the C container: %s seconds." % (end - start))
+container.remove()
+
+# WASM container
+start = time.time()
+container = cli.containers.run("dorkamotorka/hello-world-wasm", runtime="io.containerd.wasmedge.v1", platform="wasi/wasm", detach=True)
+end = time.time()
+log.info("Time took to execute the WASM container: %s seconds." % (end - start))
+container.remove()
+
+# WASM AOT container
+start = time.time()
+container = cli.containers.run("dorkamotorka/hello-world-wasm-aot", runtime="io.containerd.wasmedge.v1", platform="wasi/wasm", detach=True)
+end = time.time()
+log.info("Time took to execute the WASM AOT container: %s seconds." % (end - start))
+container.remove()
---
+++ namespaces-cgroups-etc/sleep.c
@@ -0,0 +1,8 @@
+#include <unistd.h>
+
+int main() {
+    while (1) {
+        sleep(3600);  // Sleep for 1 hour (you can adjust this as needed)
+    }
+    return 0;
+}
